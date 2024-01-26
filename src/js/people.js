c5a04765e5ea6fb18ce38b4bb13f4aba11bf4408
heda024 <143176013+heda024@users.noreply.github.com>
2024-01-26 12:30:47 +0100

    finish html people

--- src/js/people.js
+++ src/js/people.js
@@ -2,6 +2,30 @@
 // .then(response => response.json())
 // .then(data => console.log(data.results))
 
+fetch('https://swapi.dev/api/people/1')
+	.then(response => response.json())
+	.then(data => {
+		const name = document.querySelector('.movie-1 > .title');
+		const movie = document.createTextNode(data.title)
+		title.appendChild(movie)
 
+		const director = document.querySelector('.movie-1 > .dir');
+		const directorText = document.createTextNode(data.director)
+		director.appendChild(directorText)
+
+		const producer = document.querySelector('.movie-1 > .pro');
+		const producerText = document.createTextNode(data.producer)
+		producer.appendChild(producerText)
+
+		const release = document.querySelector('.movie-1 > .rel');
+		const releaseText = document.createTextNode(data.release_date)
+		release.appendChild(releaseText)
+
+		const id = document.querySelector('.movie-1 > .id');
+		const idText = document.createTextNode(data.episode_id)
+		id.appendChild(idText)
+		
+	})
+	.catch(error => console.log(error))
 
 
