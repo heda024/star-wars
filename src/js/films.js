495456a4e2e4631c8ccab3114154b74994677236
heda024 <143176013+heda024@users.noreply.github.com>
2024-01-26 12:12:53 +0100

    add javascript for movie 1

--- src/js/films.js
+++ src/js/films.js
@@ -1,47 +1,74 @@
 // fetch('https://swapi.dev/api/films')
 // .then(response => response.json())
-// .then(data => displayMovies(data.results))
-// .catch(error => console.log(error));
-
-// function displayMovies(data){
-// 	data.forEach(movie => {
-// 	console.log(movie.title);		
-// 	});
-// }
-
-// fetch('https://swapi.dev/api/films')
-// .then(response => response.json())
 // .then(data => console.log(data.results))
-// .catch(error => console.log(error));
 
+	fetch('https://swapi.dev/api/films/1')
+	.then(response => response.json())
+	.then(data => {
+		const title = document.querySelector('.movie-1 > .title');
+		const movie = document.createTextNode(data.title)
+		title.appendChild(movie)
 
+		const director = document.querySelector('.movie-1 > .dir');
+		const directorText = document.createTextNode(data.director)
+		director.appendChild(directorText)
 
-// async function checkWeather(movie){
-// 	const response = await fetch('https://swapi.dev/api/films');
-// 	let data = await response.json();
+		const producer = document.querySelector('.movie-1 > .pro');
+		const producerText = document.createTextNode(data.producer)
+		producer.appendChild(producerText)
 
-// 	console.log(data.results);
+		const release = document.querySelector('.movie-1 > .rel');
+		const releaseText = document.createTextNode(data.release_date)
+		release.appendChild(releaseText)
 
-// 	document.querySelector('.title1').innerHTML = data.[3].title;
-// }
+		const id = document.querySelector('.movie-1 > .id');
+		const idText = document.createTextNode(data.episode_id)
+		id.appendChild(idText)
+		
+	})
+	.catch(error => console.log(error))
 
-const fetchData = async ()=>{
-	const response = await fetch('https://swapi.dev/api/films');
-	const data = await response.json();
-		displayFilms(data.results)
-		pushHtml()
-	}
-	
-fetchData()
+	// fetch('https://swapi.dev/api/films/2')
+	// .then(response => response.json())
+	// .then(data => {
+	// 	const title = document.querySelector('.movie-2 > .title');
+	// 	const movie = document.createTextNode(data.title)
+	// 	title.appendChild(movie)
+	// })
+	// .catch(error => console.log(error))
 
-function displayFilms(data){
-	data.forEach(film => {
-		console.log(film.title)
-	});
-}
+	// fetch('https://swapi.dev/api/films/3')
+	// .then(response => response.json())
+	// .then(data => {
+	// 	const title = document.querySelector('.movie-3 > .title');
+	// 	const movieOne = document.createTextNode(data.title)
+	// 	title.appendChild(movie)
+	// })
+	// .catch(error => console.log(error))
 
-function pushHtml(){
-	document.querySelector('.title1').innerHTML = 'Title' + data
-}
+	// fetch('https://swapi.dev/api/films/4')
+	// .then(response => response.json())
+	// .then(data => {
+	// 	const title = document.querySelector('.movie-4 > .title');
+	// 	const movieOne = document.createTextNode(data.title)
+	// 	title.appendChild(movie)
+	// })
+	// .catch(error => console.log(error))
 
-// 
+	// fetch('https://swapi.dev/api/films/5')
+	// .then(response => response.json())
+	// .then(data => {
+	// 	const title = document.querySelector('.movie-5 > .title');
+	// 	const movieOne = document.createTextNode(data.title)
+	// 	title.appendChild(movie)
+	// })
+	// .catch(error => console.log(error))
+
+	// fetch('https://swapi.dev/api/films/6')
+	// .then(response => response.json())
+	// .then(data => {
+	// 	const title = document.querySelector('.movie-6 > .title');
+	// 	const movieOne = document.createTextNode(data.title)
+	// 	title.appendChild(movie)
+	// })
+	// .catch(error => console.log(error))
